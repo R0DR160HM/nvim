5824b0b48eda7924fdd0db6c850a12acc16785ed
Rodrigo Heinzen <rodrigo.h.m.112200@gmail.com>
2025-03-12 08:43:19 -0300

    Removing background

--- lua/rodrigo/remap.lua
+++ lua/rodrigo/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
